556dc0d76aeac9086ba2c046d0d8f2ee5647a84e
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-05-20 15:28:16 +0200

    Update ItemNemixHammer.java

--- src/main/java/com/mod/nemixmod/items/ItemNemixHammer.java
+++ src/main/java/com/mod/nemixmod/items/ItemNemixHammer.java
@@ -20,9 +20,7 @@ import org.apache.commons.logging.Log;
 import com.mod.nemixmod.init.BlockMod;
 import com.mod.nemixmod.init.ItemMod;
 
-import ibxm.Player;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -31,10 +29,10 @@ import net.minecraft.item.Item.ToolMaterial;
 
 public class ItemNemixHammer extends ItemPickaxe {
 
-
 	public ItemNemixHammer(ToolMaterial p_i45347_1_) {
 		super(p_i45347_1_);
-		// TODO Auto-generated constructor stub
+		setMaxDamage(3333);
+
 	}
 
 	public boolean onBlockDestroyed(ItemStack stack, World world, Block block, int x, int y, int z,
@@ -43,160 +41,140 @@ public class ItemNemixHammer extends ItemPickaxe {
 		boolean isStone = world.getBlock(x, y, z) == Blocks.stone;
 		boolean isStoneBrick = world.getBlock(x, y, z) == Blocks.stonebrick;
 		boolean isSandtone = world.getBlock(x, y, z) == Blocks.sandstone;
+		boolean isironstone = world.getBlock(x, y, z) == Blocks.iron_ore;
+		boolean isgoldore = world.getBlock(x, y, z) == Blocks.gold_ore;
+		boolean iscoaldore = world.getBlock(x, y, z) == Blocks.coal_ore;
+		boolean isdiamondore = world.getBlock(x, y, z) == Blocks.diamond_ore;
+		boolean isnemixore = world.getBlock(x, y, z) == BlockMod.nemix_ore;
 
-		boolean[] vanillaBlocks = new boolean[] { isCobble, isStone, isStoneBrick, isSandtone };
-		float yaw = Minecraft.getMinecraft().thePlayer.prevRenderYawOffset;
+		boolean[] vanillaBlocks = new boolean[] { isCobble, isStone, isStoneBrick, isSandtone, isironstone, isgoldore,
+				iscoaldore, isdiamondore, isnemixore };
 
-		while (yaw > 360) {
-			System.out.print("On retire un tour" + "\n");
-			yaw = yaw - 360;
-		}
+		if (elb instanceof EntityPlayer) { 										///////////////////////////////////////////////////////////////////////////
+																				// Petit patch réalisé par : github.com/antoineok | Cela évite les crash //
+			EntityPlayer player = (EntityPlayer) elb; 							// serveur donc merci à lui pour cette petite correction.				 //
+			float yaw = player.prevRenderYawOffset; 							///////////////////////////////////////////////////////////////////////////
 
-		while (yaw < 0) {
-			System.out.print("On ajoute un tour" + "\n");
-			yaw = yaw + 360;
-		}
+			while (yaw > 360) {
+				yaw = yaw - 360;
+			}
 
-		if (yaw >= 135 && yaw < 225) {
-			System.out.print("North" + yaw + "\n");
-			if (y < 6) {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-						EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
-				Minecraft.getMinecraft().thePlayer
-						.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6...."));
-			} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2]) {
-				for (int ix = -1; ix < 2; ++ix) {
-					for (int iy = -1; iy < 2; ++iy) {
-						for (int iz = -1; iz < 2; ++iz) {
+			while (yaw < 0) {
+				yaw = yaw + 360;
+			}
 
-							if (world.getBlock(x, y, z) != Blocks.bedrock) {
-								{
-									world.func_147480_a((x + ix), (y + iy), z, true);
-									stack.setItemDamage(stack.getItemDamage() - 1);
-									System.out.print("X : " + x + "\n");
-									System.out.print("IX : " + ix + "\n");
-									System.out.print("Y : " + y + "\n");
-									System.out.print("IY : " + iy + "\n");
-									System.out.print("Z : " + z + "\n");
-									System.out.print("IZ : " + iz + "\n");
+			if (yaw >= 135 && yaw < 225) { // Nord
+				if (y < 6) {
+
+					player.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6..."));
+					stack.damageItem(1, elb);
+
+				} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2] || vanillaBlocks[3]
+						|| vanillaBlocks[4] || vanillaBlocks[5] || vanillaBlocks[6] || vanillaBlocks[7]
+						|| vanillaBlocks[8]) {
+					for (int ix = -1; ix < 2; ++ix) {
+						for (int iy = -1; iy < 2; ++iy) {
+							for (int iz = -1; iz < 2; ++iz) {
+
+								if (world.getBlock(x, y, z) != Blocks.bedrock) {
+									{
+										world.func_147480_a((x + ix), (y + iy), z, true);
+									}
 								}
+
 							}
 
 						}
-
 					}
+					stack.damageItem(9, elb);
+
 				}
-			}
-		} else if (yaw >= 225 && yaw < 315) {
-			System.out.print("East" + yaw + "\n");
-			if (y < 6) {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-						EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
-				Minecraft.getMinecraft().thePlayer
-						.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6...."));
-			} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2]) {
-				for (int ix = -1; ix < 2; ++ix) {
-					for (int iy = -1; iy < 2; ++iy) {
-						for (int iz = -1; iz < 2; ++iz) {
+			} else if (yaw >= 225 && yaw < 315) { // EST
+				if (y < 6) {
 
-							if (world.getBlock(x, y, z) != Blocks.bedrock) {
-								{
-									world.func_147480_a(x, (y + iy), (z+iz), true);
-									stack.setItemDamage(stack.getItemDamage() - 1);
-									System.out.print("X : " + x + "\n");
-									System.out.print("IX : " + ix + "\n");
-									System.out.print("Y : " + y + "\n");
-									System.out.print("IY : " + iy + "\n");
-									System.out.print("Z : " + z + "\n");
-									System.out.print("IZ : " + iz + "\n");
+				} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2] || vanillaBlocks[3]
+						|| vanillaBlocks[4] || vanillaBlocks[5] || vanillaBlocks[6] || vanillaBlocks[7]
+						|| vanillaBlocks[8]) {
+					for (int ix = -1; ix < 2; ++ix) {
+						for (int iy = -1; iy < 2; ++iy) {
+							for (int iz = -1; iz < 2; ++iz) {
+
+								if (world.getBlock(x, y, z) != Blocks.bedrock) {
+									{
+										world.func_147480_a(x, (y + iy), (z + iz), true);
+									}
 								}
-							}	
 
-						}
-
-					}
-				}
-			}
-		} else if (yaw >= 46 && yaw < 135) {
-			System.out.print("West" + yaw + "\n");
-			if (y < 6) {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-						EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
-				Minecraft.getMinecraft().thePlayer
-						.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6...."));
-			} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2]) {
-				for (int ix = -1; ix < 2; ++ix) {
-					for (int iy = -1; iy < 2; ++iy) {
-						for (int iz = -1; iz < 2; ++iz) {
-
-							if (world.getBlock(x, y, z) != Blocks.bedrock) {
-								{
-									world.func_147480_a(x, y+iy, z+iz, true);
-									stack.setItemDamage(stack.getItemDamage() - 1);
-									System.out.print("X : " + x + "\n");
-									System.out.print("IX : " + ix + "\n");
-									System.out.print("Y : " + y + "\n");
-									System.out.print("IY : " + iy + "\n");
-									System.out.print("Z : " + z + "\n");
-									System.out.print("IZ : " + iz + "\n");
-									
-								}
-							}	
-
-						}
-
-					}
-				}
-			}
-			
-			
-		} else 
-		{
-			System.out.print("South" + yaw + "\n");
-			if (y < 6) {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-						EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
-				Minecraft.getMinecraft().thePlayer
-						.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6...."));
-			} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2]) {
-				for (int ix = -1; ix < 2; ++ix) {
-					for (int iy = -1; iy < 2; ++iy) {
-						for (int iz = -1; iz < 2; ++iz) {
-
-							if (world.getBlock(x, y, z) != Blocks.bedrock) {
-								{
-									world.func_147480_a((x + ix), (y + iy), z, true);
-									stack.setItemDamage(stack.getItemDamage() - 1);
-									System.out.print("X : " + x + "\n");
-									System.out.print("IX : " + ix + "\n");
-									System.out.print("Y : " + y + "\n");
-									System.out.print("IY : " + iy + "\n");
-									System.out.print("Z : " + z + "\n");
-									System.out.print("IZ : " + iz + "\n");
-								}
 							}
 
 						}
+						stack.damageItem(9, elb);
 
 					}
-				}
-			}
-			return true;
-		}
 
-				
-		stack.setItemDamage(stack.getItemDamage() - 1);
-		
+				}
+			} else if (yaw >= 46 && yaw < 135) { // OUEST
+				if (y < 6) {
+
+				} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2] || vanillaBlocks[3]
+						|| vanillaBlocks[4] || vanillaBlocks[5] || vanillaBlocks[6] || vanillaBlocks[7]
+						|| vanillaBlocks[8]) {
+					for (int ix = -1; ix < 2; ++ix) {
+						for (int iy = -1; iy < 2; ++iy) {
+							for (int iz = -1; iz < 2; ++iz) {
+
+								if (world.getBlock(x, y, z) != Blocks.bedrock) {
+									{
+										world.func_147480_a(x, y + iy, z + iz, true);
+
+									}
+								}
+
+							}
+
+						}
+					}
+					stack.damageItem(9, elb);
+
+				}
+
+			} else // SUD
+			{
+				if (y < 6) {
+					player.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GOLD + "Tu ne peut pas utiliser le nemix hammer en desous de la"));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "couche 6..."));
+					stack.damageItem(1, elb);
+				} else if (vanillaBlocks[0] || vanillaBlocks[1] || vanillaBlocks[2] || vanillaBlocks[3]
+						|| vanillaBlocks[4] || vanillaBlocks[5] || vanillaBlocks[6] || vanillaBlocks[7]
+						|| vanillaBlocks[8]) {
+					for (int ix = -1; ix < 2; ++ix) {
+						for (int iy = -1; iy < 2; ++iy) {
+							for (int iz = -1; iz < 2; ++iz) {
+
+								if (world.getBlock(x, y, z) != Blocks.bedrock) {
+									{
+										world.func_147480_a((x + ix), (y + iy), z, true);
+
+									}
+								}
+
+							}
+
+						}
+					}
+					stack.damageItem(9, elb);
+				}
+
+				return true;
+			}
+
+		}
 		return false;
 	}
-	
-
-
 }
-
-
-
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //																																																	//
